7bfdeefc2908494a35faf56dbc1440d50691ce30
AminBabaei <aminbabaei_dev@yahoo.com>
2026-02-16 11:24:17 +0330

    fix(schema): validation password 6 character

--- app/lib/schemas/auth/login.schema.ts
+++ app/lib/schemas/auth/login.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const loginSchema = z.object({
   email: z.email({ message: "ایمیل اشتباه وارد کردی!" }),
-  password: z.string().min(5, { message: "پسورد حداقل 6 کاراکتر باشد" }),
+  password: z.string().min(6, { message: "پسورد حداقل 6 کاراکتر باشد" }),
 });
 
 export type LoginFormData = z.infer<typeof loginSchema>;
